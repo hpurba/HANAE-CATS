7ab91a9cf8555dd36bd18d9fb80397b56d76afc6
hpurba <hikaru.purba@gmail.com>
2020-02-22 12:47:42 -0700

    got the API working, next is work on the formatting of the page

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("NewCatPictureSubmit").addEventListener("click", function(event) {
+event.preventDefault();
+// const value = document.getElementById("weatherInput").value;
+// if (value === "")
+//   return;
+// console.log(value);
+
+const url = "https://api.thecatapi.com/v1/images/search";
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  }).then(function(json) {
+
+    // console.log to show what is in the json
+    console.log(json);
+
+    let results = "";
+    results += '<img src = "';
+    results += json[0].url;
+    results += '"/>';
+
+    // results += '<img src="' + json[0].url + '"/>';
+    document.getElementById("NewCatPictureResult").innerHTML = results;
+
+  });
+});
